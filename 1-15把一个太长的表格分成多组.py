6d2a8a1ea5fbd7f271d88c59f5db4bbeb1d01aed
张颖俊 <16438429+dx261@user.noreply.gitee.com>
2026-02-24 20:17:17 +0800

    Changes

--- 1-15把一个太长的表格分成多组.py
+++ 1-15把一个太长的表格分成多组.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pandas as pd
 
-df = pd.read_excel("双元素步长2%样本.xlsx")
+df = pd.read_excel("2-24外推元素高通量样本4旧+1新.xlsx")
 
-for i, part in enumerate(np.array_split(df, 10), start=1):
-    part.to_excel(f"双元素步长2%样本part{i}.xlsx", index=False)
+for i, part in enumerate(np.array_split(df, 40), start=1):
+    part.to_excel(f"2-24data/2-24外推元素高通量样本4旧+1新part{i}.xlsx", index=False)
 
