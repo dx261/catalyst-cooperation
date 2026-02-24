1fa4e2c6a1521a57c11338fda4835b54d733664f
张颖俊 <16438429+dx261@user.noreply.gitee.com>
2026-02-24 20:38:09 +0800

    Changes

--- 11-10提取matminer特征.py
+++ 11-10提取matminer特征.py
@@ -1,3 +1,5 @@
+import os
+
 import pandas as pd
 import numpy as np
 import itertools
@@ -14,10 +16,13 @@ if __name__ == '__main__':
     # df = pd.read_excel(f"双元素步长2%样本{part}.xlsx")
 
     df = pd.read_excel("2-24外推元素高通量样本4旧+1新.xlsx")
-    df = StrToComposition(reduce=True, target_col_id='composition_obj').featurize_dataframe(df, 'formula')
-    feature_calculators = MultipleFeaturizer([cf.Stoichiometry(), cf.ElementProperty.from_preset("magpie"),
-                                              WenAlloys()])
-    feature_labels = feature_calculators.feature_labels()
-    data = feature_calculators.featurize_dataframe(df, col_id='composition_obj', ignore_errors=True)
-    # data.to_excel(f"data/双元素步长2%样本-提取matminer特征{part}.xlsx")
-    data.to_excel("2-24外推元素高通量样本4旧+1新-提取matminer特征.xlsx")
+    for i, file in enumerate(os.listdir("2-24data")):
+        print(file)
+        df = pd.read_excel(f"2-24data/{file}")
+        df = StrToComposition(reduce=True, target_col_id='composition_obj').featurize_dataframe(df, 'formula')
+        feature_calculators = MultipleFeaturizer([cf.Stoichiometry(), cf.ElementProperty.from_preset("magpie"),
+                                                  WenAlloys()])
+        feature_labels = feature_calculators.feature_labels()
+        data = feature_calculators.featurize_dataframe(df, col_id='composition_obj', ignore_errors=True)
+        # data.to_excel(f"data/双元素步长2%样本-提取matminer特征{part}.xlsx")
+        data.to_excel(f"2-24外推元素高通量样本4旧+1新-提取matminer特征_{i}.xlsx")
